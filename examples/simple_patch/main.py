b88d6c856a746e9bef0baee4d2114557d04a0579
Danny Hermes <daniel.j.hermes@gmail.com>
2014-11-03 11:03:43 -0800

    Basic working app (before hacks).

---
+++ examples/simple_patch/main.py
@@ -0,0 +1,46 @@
+import endpoints
+
+from google.appengine.ext import ndb
+from protorpc import remote
+
+from endpoints_proto_datastore.ndb import EndpointsModel
+
+
+class MyModel(EndpointsModel):
+  _message_fields_schema = ('id', 'attr1', 'created')
+
+  attr1 = ndb.StringProperty(repeated=True)
+  created = ndb.DateTimeProperty(auto_now_add=True)
+
+
+DEFAULT_FIELDS = MyModel._DefaultFields()
+
+
+@endpoints.api(name='myapi', version='v1', description='My Little API')
+class MyApi(remote.Service):
+
+  @MyModel.method(request_fields=DEFAULT_FIELDS,
+                  path='mymodel', http_method='POST', name='mymodel.insert')
+  def MyModelInsert(self, my_model):
+    my_model.put()
+    return my_model
+
+  @MyModel.method(path='mymodel/{id}', request_fields=DEFAULT_FIELDS,
+                  http_method='PATCH', name='mymodel.update')
+  def MyModelPatch(self, my_model):
+    my_model.put()
+    return my_model
+
+  @MyModel.method(request_fields=('id',),
+                  path='mymodel/{id}', http_method='GET', name='mymodel.get')
+  def MyModelGet(self, my_model):
+    if not my_model.from_datastore:
+      raise endpoints.NotFoundException('MyModel not found.')
+    return my_model
+
+  @MyModel.query_method(path='mymodels', name='mymodel.list')
+  def MyModelList(self, query):
+    return query
+
+
+application = endpoints.api_server([MyApi], restricted=False)
